e98670e5729829af71b8512bc40453d233291b84
Chiiip <pedro_pianista@hotmail.com>
2020-01-30 14:20:50 -0300

    initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import React, { Suspense, lazy } from 'react';
+import { Router } from "@reach/router"
+
+const Home = lazy(() => import('./pages/Home'));
+const FormPage = lazy(() => import('./pages/FormPage'));
+
+function App() {
+  return (
+    <Suspense fallback={<div>Carregando...</div>}>
+    <Router>
+        <Home path="/" />
+        <FormPage path="form" />
+    </Router>
+    </Suspense>
+  );
+}
+
+export default App;
