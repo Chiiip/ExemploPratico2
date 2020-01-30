e1bdcddcb4db326f8402f3f035cc8177efe9c6ab
Chiiip <pedro_pianista@hotmail.com>
2020-01-30 14:22:00 -0300

    adding import

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,7 @@
 import React, { Suspense, lazy } from 'react';
 import { Router } from "@reach/router"
+// import Home from "./pages/Home";
+// import FormPage from "./pages/FormPage";
 
 const Home = lazy(() => import('./pages/Home'));
 const FormPage = lazy(() => import('./pages/FormPage'));
